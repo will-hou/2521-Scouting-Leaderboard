9695b1e7ac140fb7a2445ae14be954cad6300913
William <whou125@gmail.com>
2019-02-09 14:54:42 -0800

    Make medal styling more DRY

--- src/components/LeaderboardRow.js
+++ src/components/LeaderboardRow.js
@@ -3,26 +3,25 @@ import CustomTableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
 
 import '../styles/Leaderboard.css'
-import { bold } from 'ansi-colors';
 
 export class LeaderboardRow extends Component {
 
 
-  color(position) {
+  getMedalColor(position) {
     switch (position)  {
-      case 1: return <span className='medal-gold'>{this.props.index}</span>
-      case 2: return <span className='medal-silver'>{this.props.index}</span>
-      case 3: return <span className='medal-bronze'>{this.props.index}</span>
-      default: return <span className='medal-other'>{this.props.index}</span>
+      case 1: return <span className='medal medal-gold'>{this.props.index}</span>
+      case 2: return <span className='medal medal-silver'>{this.props.index}</span>
+      case 3: return <span className='medal medal-bronze'>{this.props.index}</span>
+      default: return <span className='medal medal-other'>{this.props.index}</span>
     }
   }
   
   render() {
       return(
       <TableRow className= 'TableRow'>
-        <CustomTableCell className='LeaderboardPosition'>{this.color(this.props.index)}</CustomTableCell>
-        <CustomTableCell>{this.props.name}</CustomTableCell>
-        <CustomTableCell className='LeaderboardRow' align='left'>{this.props.scouts}</CustomTableCell>
+        <CustomTableCell className='LeaderboardPosition'>{this.getMedalColor(this.props.index)}</CustomTableCell>
+        <CustomTableCell><span className='LeaderboardText'><b>{this.props.name}</b></span></CustomTableCell>
+        <CustomTableCell className='LeaderboardRow' align='left'><span className='LeaderboardText'>{this.props.scouts}</span></CustomTableCell>
       </TableRow>
       )
     }
