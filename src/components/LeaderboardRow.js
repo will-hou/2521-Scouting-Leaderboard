8645e18c20b2d4491c8f25725a08cf95d44b9462
William <whou125@gmail.com>
2019-02-08 21:38:31 -0800

    Add medals for top three positions

--- src/components/LeaderboardRow.js
+++ src/components/LeaderboardRow.js
@@ -6,10 +6,21 @@ import '../styles/Leaderboard.css'
 import { bold } from 'ansi-colors';
 
 export class LeaderboardRow extends Component {
-    render() {
+
+
+  color(position) {
+    switch (position)  {
+      case 1: return <span className='medal-gold'>{this.props.index}</span>
+      case 2: return <span className='medal-silver'>{this.props.index}</span>
+      case 3: return <span className='medal-bronze'>{this.props.index}</span>
+      default: return <span className='medal-other'>{this.props.index}</span>
+    }
+  }
+  
+  render() {
       return(
       <TableRow className= 'TableRow'>
-        <CustomTableCell className='LeaderboardPosition'><b>{this.props.index}</b></CustomTableCell>
+        <CustomTableCell className='LeaderboardPosition'>{this.color(this.props.index)}</CustomTableCell>
         <CustomTableCell>{this.props.name}</CustomTableCell>
         <CustomTableCell className='LeaderboardRow' align='left'>{this.props.scouts}</CustomTableCell>
       </TableRow>
