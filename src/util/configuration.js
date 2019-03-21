db30751f817b5dfb3ef2a1f61acbd6539bff24ff
William <whou125@gmail.com>
2019-03-21 16:52:58 -0700

    Update with all Clackams scout data

--- src/util/configuration.js
+++ src/util/configuration.js
@@ -2,18 +2,9 @@ const configuration = {
     // The metric associated with the Scout's Name 
     metricName: 'Scout Name',
     // The url location of the scouting data
-    // url: 'https://api.jsonbin.io/b/5c50f65da3fb18257ac82555',
-    //Clackams Day 1 url: 
-    // url: 'https://jsonstorage.net/api/items/c8969c12-692c-4d66-beb6-b63af99585e9',
-    url: 'https://jsonstorage.net/api/items/64c24ad3-1052-44d5-9482-3532da999cdd',
-
+    url: 'https://jsonstorage.net/api/items/6a164662-476d-42cc-bc70-d3f897e7724d',
     // When the leaderboard was last updated
-    lastUpdated: 'Clackamas Day 2 (Before Q65)'
-
-    // Data
-    // 'https://jsonstorage.net/api/items/abe32013-f3a4-41c4-b77a-f4b9091d5a86' //scout1_test_combined.json
-    // Non Houston/Wilsonville: url: 'https://jsonstorage.net/api/items/dfd580d4-e3bf-416a-a695-810b196d41ba'
-    // Houston w/ No Wilsonville: 'https://jsonstorage.net/api/items/cd10d9ec-dc6c-4a22-a836-31d6a4438761'
+    lastUpdated: 'Clackams'
 }
 
 export { configuration }
