438a05fca8f67ad076efe2323c7ac758d2cb2a14
William <whou125@gmail.com>
2020-02-29 09:23:49 -0800

    Add 2020 clackams day 1 scouts

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Table from '@material-ui/core/Table';
 import {LeaderboardRow} from './LeaderboardRow'
 import TableBody from '@material-ui/core/TableBody';
@@ -10,59 +10,61 @@ import formatJson from '../util/formatJson.js'
 import "../styles/Leaderboard.css"
 import Paper from '@material-ui/core/Paper'
 
-import { configuration } from '../util/configuration.js'
+import {configuration} from '../util/configuration.js'
 
 class Leaderboard extends Component {
 
     state = {
-      rows: []
+        rows: []
     }
 
     createRows(data) {
         const rows = []
-        
+
         data.forEach(person => {
-          rows.push(
-            <LeaderboardRow key={person[0]} index={data.indexOf(person) + 1} name={person[0]} scouts={person[1]}/>
-          )
+            rows.push(
+                <LeaderboardRow key={person[0]} index={data.indexOf(person) + 1} name={person[0]} scouts={person[1]}/>
+            )
         })
         return rows
-      }
+    }
 
-  componentDidMount() {
-    // Fetch and format JSON data
-    // fetch(configuration.url)
-    // .then(response => response.json())
-    // .then(data => {
-    //   const formatted_data = formatJson(data)
-    //   const rows = this.createRows(formatted_data)
-    //   this.setState({rows: rows})
-    // })
-    var json_data = require('../2019_match_scouts.json')
-    const formatted_data = formatJson(json_data)
-    const rows = this.createRows(formatted_data)
-    this.setState({rows: rows})
+    componentDidMount() {
+        // Fetch and format JSON data
+        // fetch(configuration.url)
+        // .then(response => response.json())
+        // .then(data => {
+        //   const formatted_data = formatJson(data)
+        //   const rows = this.createRows(formatted_data)
+        //   this.setState({rows: rows})
+        // })
+        var json_data = require('../2020_match_scouts.json')
+        const formatted_data = formatJson(json_data)
+        const rows = this.createRows(formatted_data)
+        this.setState({rows: rows})
 
-  }
+    }
 
-  render() {  
-    return (
-    <Paper className='Leaderboard' elevation={9}>
-        <Table className='Leaderboard'>
-            <TableHead>
-            <TableRow>
-                <CustomTableCell align='left'></CustomTableCell>
-                <CustomTableCell align='left'><span class='LeaderboardHeaderText'>Scouter Name</span></CustomTableCell>
-                <CustomTableCell align='left'><span class='LeaderboardHeaderText'>Number of Scouts</span></CustomTableCell>
-            </TableRow>
-            </TableHead>
-            <TableBody>
-              {this.state.rows}
-            </TableBody>
-        </Table>
-    </Paper>
-    )
-  }
+    render() {
+        return (
+            <Paper className='Leaderboard' elevation={9}>
+                <Table className='Leaderboard'>
+                    <TableHead>
+                        <TableRow>
+                            <CustomTableCell align='left'></CustomTableCell>
+                            <CustomTableCell align='left'><span
+                                class='LeaderboardHeaderText'>Scouter Name</span></CustomTableCell>
+                            <CustomTableCell align='left'><span
+                                class='LeaderboardHeaderText'>Number of Scouts</span></CustomTableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {this.state.rows}
+                    </TableBody>
+                </Table>
+            </Paper>
+        )
+    }
 }
 
 export default Leaderboard;
--- src/util/configuration.js
+++ src/util/configuration.js
@@ -1,9 +1,9 @@
 const configuration = {
     // The metric associated with the Scout's Name 
-    metricName: 'Scout Name',
+    metricName: 'Scout name',
     // The url location of the scouting data
     // url: 'https://jsonstorage.net/api/items/6a164662-476d-42cc-bc70-d3f897e7724d',
     // When the leaderboard was last updated
-    lastUpdated: 'PNW District Championships'
+    lastUpdated: 'Clackams Day 1'
 }
 export { configuration }
